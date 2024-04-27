87ef9934d75b843acfea460bd1d29324eb7a318e
weverttongg <weverttonrodrigues09@gmail.com>
2024-04-26 21:38:20 -0300

    Adicionando uma classe !!

---
+++ src/WEVERTTON_RODRIGUES.java
@@ -0,0 +1,12 @@
+
+public class WEVERTTON_RODRIGUES {
+
+    
+    public static void main(String[] args) {
+        System.err.println("[ OLÁ PROFESSOR MARCELO ]");
+        System.out.println("Meu nome é Wevertton da turma 2J");
+        System.out.println("Esta e a minha inclusão de codigo JAVA");
+        
+    }
+
+}
