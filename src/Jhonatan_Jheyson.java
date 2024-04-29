3e94662a7852bef9d2be53741920bf62a8146175
Jhon-2024 <167825048+Jhon-2024@users.noreply.github.com>
2024-04-29 20:46:23 -0300

    Alterei o projeto Carro

--- src/Jhonatan_Jheyson.java
+++ src/Jhonatan_Jheyson.java
@@ -1,8 +1,13 @@
 
+import java.util.Scanner;
+
+
 
 public class Jhonatan_Jheyson {
  
-
+    String modelo;
+    String marca;
+    int ano;
     public static void main(String[] args) {
         System.out.println(" Olá Marcelo ! ");
         System.out.println(" Jhonatan Jheyson ");
@@ -11,5 +16,19 @@ public class Jhonatan_Jheyson {
         System.out.println(" Quero acerta esse trem ");
 }
     
-    
+    void exibirInfo() {
+        System.out.println("Marca: " + marca);
+        System.out.println("Modelo: " + modelo);
+        System.out.println("Ano: " + ano);
+        System.out.println("--------------------");
+}
+    void solicitarInfo() {
+        Scanner leia = new Scanner(System.in);
+        System.out.print("Digite a marca do veículo: ");
+        marca = leia.nextLine();
+        System.out.print("Digite o modelo: ");
+        modelo = leia.nextLine();
+        System.out.print("Digite o ano: ");
+        ano = leia.nextInt();  
+}
 }
