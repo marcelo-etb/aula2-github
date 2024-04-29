74d1c0019e01eba76448a3719519e5887b8ba67d
Daniel Gouveia Vasconcelos <dev.danielvasconcelos@gmail.com>
2024-04-29 20:39:37 -0300

    Create DanielVasconcelos.java

---
+++ src/DanielVasconcelos.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class DanielVasconcelos {
+    
+    public static void main(String[] args) {
+        
+        Scanner leia = new Scanner(System.in);
+        
+        double a1, a2, a3, media; 
+        
+        System.out.println("Saiba a média das suas notas do Curso técnico em Informática !");
+        System.out.println("Informe as suas notas ");
+        System.out.print("Nota A1: ");
+        a1 = leia.nextDouble();
+        System.out.print("Nota A2: ");
+        a2 = leia.nextDouble();
+        System.out.print("Nota A3: ");
+        a3 = leia.nextDouble();
+       
+        a2 = a2 * 2;
+        a3 = a3 * 2;
+        media = (a1 + a2 + a3) / 5;
+        
+        System.out.println(media);
+        
+    }
+}
