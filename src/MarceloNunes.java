7bc6f3ff413149f2cad6e2d413ee16f839d66539
Masello01 <167825388+Masello01@users.noreply.github.com>
2024-04-29 20:33:35 -0300

    deu certo agora

--- src/MarceloNunes.java
+++ src/MarceloNunes.java
@@ -4,6 +4,7 @@ public class MarceloNunes {
         System.out.println("Olá aluno");
         System.out.println("Crie uma classe com seu nome e sobrenome");
         System.out.println("Salve e me envie o resultado!");
+        System.out.println("DEU CERTO DENOVO");
     }
 
 }
