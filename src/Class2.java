f3084e805b20e31f57af98dfee71dbb4d60ae8df
Masello01 <167825388+Masello01@users.noreply.github.com>
2024-04-29 20:30:38 -0300

    alteração class2

--- src/Class2.java
+++ src/Class2.java
@@ -2,6 +2,6 @@
 public class Class2 {
     public static void main(String[] args) {
         System.out.println("teste da class 2 - file");
-       
+        System.out.println("DEU CERTO!");
     }
 }
