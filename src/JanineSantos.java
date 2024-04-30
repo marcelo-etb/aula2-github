79aa2f7e650ce21065ff4aa73fb32e06fb0a36ea
Janinebsb <167825133+Janinebsb@users.noreply.github.com>
2024-04-29 21:18:09 -0300

    Não fiz nada

---
+++ src/JanineSantos.java
@@ -0,0 +1,10 @@
+
+public class JanineSantos {
+    
+    public static void main (String[] args) {
+        
+        System.out.println("Boa Noite!");
+        System.out.println("SOS");
+        System.out.println("");
+    }
+}
