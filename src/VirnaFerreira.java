5df05cadb522cd4382d41b71652c82edc46e48fa
VirnaLucia <167825131+VirnaLucia@users.noreply.github.com>
2024-04-29 21:17:32 -0300

    Nada feito

---
+++ src/VirnaFerreira.java
@@ -0,0 +1,13 @@
+
+public class VirnaFerreira {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Ola!!!!!");
+        System.out.println("Tudo bem?");
+        System.out.println("Parabens! Você acaba de receber uma benção de Deus");
+        
+        
+    }
+    
+}
