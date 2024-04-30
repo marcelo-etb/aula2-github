bf4baa5438414c040dda33fbc28abae6958acb34
RonaAlex <167825102+RonaAlex@users.noreply.github.com>
2024-04-29 21:21:35 -0300

    To so testando
    Segue so um teste

---
+++ src/soumteste.java
@@ -0,0 +1,13 @@
+public class soumteste {
+public static void main(String[] args) {
+   System.out.println("Boa Noite Professor");    
+        System.out.println("E SO UM TESTE ");
+        System.out.println(" ");
+        System.out.println(" ");
+        
+        
+        
+        
+    }
+
+}
